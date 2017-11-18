2dc8985f08891fa56fb9e61833cfd3599776ff92
GodofGurus <eng.abdulrahim.charif@gmail.com>
2017-11-18 17:36:18 -0500

    adding ios 11.1.1 client for Wi-Fi Shimes

---
+++ CameraCop-ios-client/CameraCop/CameraCop/CameraCop-Bridging-Header.h
@@ -0,0 +1,16 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+
+
+#ifndef CameraCop_Bridging_Header
+#define CameraCop_Bridging_Header
+#import "MMLANScanner.h"
+#import "LANProperties.h"
+#import "PingOperation.h"
+#import "MMLANScanner.h"
+#import "MACOperation.h"
+#import "MacFinder.h"
+#import "MMDevice.h"
+#endif /* CameraCop_Bridging_Header */
